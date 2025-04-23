1255602ab981068a47e28a816cbf35d7951932d8
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-23 13:06:45 +0300

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -11,15 +11,28 @@ namespace Snake
     class Point
     {
         // Поля класса (данные, которые хранит точка)
-        public int x;   // Координата X точки
-        public int y;   // Координата Y точки
-        public char sym; // Символ для отображения точки
+        public int x;   
+        public int y; 
+        public char sym; 
 
-        // Метод (функция) класса для отрисовки точки Метод принадлежит объекту класса Point и работает с его полями (x, y, sym) не static, так как вызывается для конкретного объекта (p1.Draw(), p2.Draw())
+        // Конструктор по умолчанию (без параметров)Вызывается, если создать объект так: new Point() в C# он создается неявно, если нет других конструкторов.
+        public Point()
+        {
+        }
+
+
+        public Point(int _x, int _y, char _sym)
+        {
+            x = _x;   
+            y = _y;   
+            sym = _sym;
+        }
+
+        // Метод класса для отрисовки точки
         public void Draw()
         {
-            Console.SetCursorPosition(x, y); // Устанавливаем курсор в координаты точки (x, y)
-            Console.Write(sym);            // Выводим символ точки (sym)
+            Console.SetCursorPosition(x, y); 
+            Console.Write(sym);            // Выводим символ точки
         }
     }
 }
