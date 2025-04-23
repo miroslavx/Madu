b28d10249916521478ada2b3ff0b7afa7cf04613
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-23 13:11:04 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-// Program.cs
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,37 +11,33 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-
             Point p1 = new Point(1, 3, '*');
+            p1.Draw();
 
-            p1.Draw(); 
             Point p2 = new Point(4, 5, '#');
+            p2.Draw();
 
-            p2.Draw(); 
+            List<int> numList = new List<int>();
+            numList.Add(0);
+            numList.Add(1);
+            numList.Add(2);
 
-            /* 
-            /*
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
-            Draw(x1, y1, sym1);
+            int x = numList[0];
+            int y = numList[1];
+            int z = numList[2];
 
-            int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-            Draw(x2, y2, sym2);
-            */
+            foreach (int i in numList)
+            {
+                Console.WriteLine(i);
+            }
 
-            Console.ReadLine(); // Ожидаем нажатия Enter
+            numList.RemoveAt(0);
+
+            List<Point> pList = new List<Point>();
+            pList.Add(p1);
+            pList.Add(p2);
+
+            Console.ReadLine();
         }
-
-        /* 
-        /*
-        static void Draw(int x, int y, char sym)
-        {
-            Console.SetCursorPosition(x, y);
-            Console.Write(sym);
-        }
-        */
     }
 }
