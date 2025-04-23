5c2302fbecc96707b6a89c744c5cff85b4b51856
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-23 13:10:38 +0300

    Update Point.cs

--- Point.cs
+++ Point.cs
@@ -7,32 +7,27 @@ using System.Threading.Tasks;
 
 namespace Snake
 {
-    // Класс Point определяет новый тип данных для точки на экране
     class Point
     {
-        // Поля класса (данные, которые хранит точка)
-        public int x;   
-        public int y; 
-        public char sym; 
+        public int x;
+        public int y;
+        public char sym;
 
-        // Конструктор по умолчанию (без параметров)Вызывается, если создать объект так: new Point() в C# он создается неявно, если нет других конструкторов.
         public Point()
         {
         }
 
-
         public Point(int _x, int _y, char _sym)
         {
-            x = _x;   
-            y = _y;   
+            x = _x;
+            y = _y;
             sym = _sym;
         }
 
-        // Метод класса для отрисовки точки
         public void Draw()
         {
-            Console.SetCursorPosition(x, y); 
-            Console.Write(sym);            // Выводим символ точки
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
