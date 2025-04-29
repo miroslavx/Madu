8d50a70c6068afef63015f084491182cc1d6c2f0
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-29 17:23:35 +0300

    Update HorizontalLine.cs

--- HorizontalLine.cs
+++ HorizontalLine.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-
 namespace Snake
 {
     class HorizontalLine : Figure
-    {
-        public HorizontalLine(int xLeft, int xRight, int y, char sym)
+    {public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
             pList = new List<Point>();
             for (int x = xLeft; x <= xRight; x++)
@@ -13,6 +11,14 @@ namespace Snake
                 Point p = new Point(x, y, sym);
                 pList.Add(p);
             }
+        }
+        public override void Draw()
+        {Console.ForegroundColor = ConsoleColor.Yellow;
+            foreach (Point p in pList)
+            {
+                p.Draw();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
