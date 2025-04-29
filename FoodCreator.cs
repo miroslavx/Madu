1737ec1b6a50d0fd1ee04b9950a2cf859a0566df
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-29 18:50:14 +0300

    Update FoodCreator.cs

--- FoodCreator.cs
+++ FoodCreator.cs
@@ -1,19 +1,17 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Snake
 {
+    // Класс, отвечающий за создание еды в случайном месте карты
     class FoodCreator
     {
-        int mapWidht;
-        int mapHeight;
-        char sym;
+        int mapWidht;  // Ширина карты
+        int mapHeight; // Высота карты
+        char sym;      // Символ для еды
 
-        Random random = new Random();
+        Random random = new Random(); // Генератор случайных чисел
 
+        // Конструктор сохраняет размеры карты и символ еды
         public FoodCreator(int mapWidth, int mapHeight, char sym)
         {
             this.mapWidht = mapWidth;
@@ -21,11 +19,13 @@ namespace Snake
             this.sym = sym;
         }
 
+        // Создает и возвращает точку еды в случайных координатах в пределах карты
         public Point CreateFood()
         {
+            // Генерируем координаты, отступая от краев карты (индекс 2, чтобы не появляться на стенах)
             int x = random.Next(2, mapWidht - 2);
             int y = random.Next(2, mapHeight - 2);
-            return new Point(x, y, sym);
+            return new Point(x, y, sym); // Возвращаем новую точку
         }
     }
 }
