619086e529fb629e78283cd82c41348be5e947b7
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-29 17:26:28 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,30 +14,61 @@ namespace Snake
             Console.SetBufferSize(80, 25);
             Console.CursorVisible = false;
 
-            HorizontalLine upLine = new HorizontalLine(0, 78, 0, '+');
-            HorizontalLine downLine = new HorizontalLine(0, 78, 24, '+');
-            VerticalLine leftLine = new VerticalLine(0, 24, 0, '+');
-            VerticalLine rightLine = new VerticalLine(0, 24, 78, '+');
-
-            upLine.Draw();
-            downLine.Draw();
-            leftLine.Draw();
-            rightLine.Draw();
+            Walls walls = new Walls(80, 25);
+            walls.Draw();
 
             Point p = new Point(4, 5, '*');
             Snake snake = new Snake(p, 4, Direction.RIGHT);
             snake.Draw();
 
-            while(true)
+            FoodCreator foodCreator = new FoodCreator(80, 25, '$');
+            Point food = foodCreator.CreateFood();
+            food.Draw();
+
+            while (true)
             {
+                if (walls.IsHit(snake) || snake.IsHitTail())
+                {
+                    break;
+                }
+
+                if(snake.Eat( food ) )
+                {
+                    food = foodCreator.CreateFood();
+                    food.Draw();
+                }
+                else
+                {
+                    snake.Move();
+                }
+
+                Thread.Sleep(100);
+
                 if (Console.KeyAvailable)
                 {
-                    ConsoleKeyInfo keyInfo = Console.ReadKey();
-                    snake.HandleKey( keyInfo.Key );
+                    ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+                    snake.HandleKey(keyInfo.Key);
                 }
-                Thread.Sleep(100);
-                snake.Move();
             }
+            WriteGameOver();
+            Console.ReadLine();
+        }
+        static void WriteGameOver()
+        {
+            int xOffset = 25;
+            int yOffset = 8;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.SetCursorPosition(xOffset, yOffset++);
+            WriteText("============================", xOffset, yOffset++);
+            WriteText("        И Г Р А    О К О Н Ч Е Н А", xOffset + 1, yOffset++);
+            yOffset++;
+            WriteText("============================", xOffset, yOffset++);
+        }
+
+        static void WriteText( String text, int xOffset, int yOffset )
+        {
+            Console.SetCursorPosition(xOffset, yOffset);
+            Console.WriteLine( text );
         }
     }
 }
