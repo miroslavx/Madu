f03d508b3baa0e0950d28f4c4b13bbde4690a815
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-04-29 18:56:18 +0300

    Update FoodCreator.cs

--- FoodCreator.cs
+++ FoodCreator.cs
@@ -1,31 +1,41 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
-    // Класс, отвечающий за создание еды в случайном месте карты
+    // Класс, отвечающий за создание еды на игровом поле
     class FoodCreator
     {
-        int mapWidht;  // Ширина карты
-        int mapHeight; // Высота карты
-        char sym;      // Символ для еды
+        // Ширина игрового поля
+        int mapWidht;
+        // Высота игрового поля
+        int mapHeight;
+        // Символ, которым отображается еда
+        char sym;
 
-        Random random = new Random(); // Генератор случайных чисел
+        // Генератор случайных чисел для определения координат еды
+        Random random = new Random();
 
-        // Конструктор сохраняет размеры карты и символ еды
+        // Конструктор класса, принимающий размеры карты и символ еды
         public FoodCreator(int mapWidth, int mapHeight, char sym)
         {
-            this.mapWidht = mapWidth;
-            this.mapHeight = mapHeight;
-            this.sym = sym;
+            this.mapWidht = mapWidth;   // Сохраняем ширину
+            this.mapHeight = mapHeight; // Сохраняем высоту
+            this.sym = sym;             // Сохраняем символ
         }
 
-        // Создает и возвращает точку еды в случайных координатах в пределах карты
+        // Метод для создания точки еды в случайном месте карты (внутри границ)
         public Point CreateFood()
         {
-            // Генерируем координаты, отступая от краев карты (индекс 2, чтобы не появляться на стенах)
+            // Генерируем случайную X координату (с отступом от краев)
             int x = random.Next(2, mapWidht - 2);
+            // Генерируем случайную Y координату (с отступом от краев)
             int y = random.Next(2, mapHeight - 2);
-            return new Point(x, y, sym); // Возвращаем новую точку
+            // Возвращаем новую точку с вычисленными координатами и заданным символом
+            return new Point(x, y, sym);
         }
     }
 }
