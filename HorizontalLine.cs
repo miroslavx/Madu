74e4d81bbc99a0aefb20e50002dc51f85f33e269
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-05-06 19:16:59 +0300

    Update HorizontalLine.cs

--- HorizontalLine.cs
+++ HorizontalLine.cs
@@ -3,30 +3,17 @@ using System.Collections.Generic;
 
 namespace Snake
 {
-    // Класс для представления горизонтальной линии, наследуется от Figure
+    // Класс HorizontalLine: Представляет горизонтальную линию, наследуется от Figure.
     class HorizontalLine : Figure
     {
-        // Конструктор для создания горизонтальной линии
         public HorizontalLine(int xLeft, int xRight, int y, char sym)
         {
-            pList = new List<Point>(); // Инициализация списка точек
-            for (int x = xLeft; x <= xRight; x++) // Цикл для создания точек линии
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xRight; x++)
             {
-                Point p = new Point(x, y, sym); // Создание точки
-                pList.Add(p);                  // Добавление точки в список
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
             }
-        }
-
-        // Переопределенный метод отрисовки для горизонтальной линии (рисует желтым цветом)
-        public override void Draw()
-        {
-            Console.ForegroundColor = ConsoleColor.Yellow; // Устанавливаем желтый цвет
-            foreach (Point p in pList) // Отрисовываем каждую точку линии
-            {
-                p.Draw();
-            }
-            // base.Draw(); // Можно вызвать базовую реализацию, если нужно (здесь не используется)
-            Console.ForegroundColor = ConsoleColor.White; // Возвращаем цвет консоли по умолчанию
         }
     }
 }
