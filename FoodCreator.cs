5ade26bafa9ef143a5a533323eef71d64a13ffe3
miroslavx <56218543+miroslavx@users.noreply.github.com>
2025-05-13 18:29:54 +0300

    Update FoodCreator.cs

--- FoodCreator.cs
+++ FoodCreator.cs
@@ -18,18 +18,21 @@ namespace Snake
             sym = foodSymbol;
         }
 
+        // Создает объект еды в случайном свободном месте на карте.
         public Point CreateFood(List<Point> snakeBody, Point currentScissors, List<Figure> obstacles)
         {
             int x, y;
             bool collision;
             Point newFoodLocation;
-            do
+            do // для гарантии, что еда не появится на занятом месте
             {
+                // Генерация случайных координат в пределах игрового поля (исключая границы)
                 x = random.Next(1, mapWidth - 1);
                 y = random.Next(1, mapHeight - 1);
                 newFoodLocation = new Point(x, y, sym);
                 collision = false;
 
+                // Проверка на столкновение с телом змейки
                 if (snakeBody != null)
                 {
                     foreach (Point p in snakeBody)
@@ -37,15 +40,17 @@ namespace Snake
                         if (p.IsHit(newFoodLocation)) { collision = true; break; }
                     }
                 }
+                // Проверка на столкновение с ножницами
                 if (!collision && currentScissors != null && currentScissors.IsHit(newFoodLocation))
                 {
                     collision = true;
                 }
+                // Проверка на столкновение с другими препятствиями
                 if (!collision && obstacles != null)
                 {
                     foreach (Figure obs in obstacles)
                     {
-                        if (obs.ContainsPoint(newFoodLocation))
+                        if (obs.ContainsPoint(newFoodLocation)) 
                         {
                             collision = true;
                             break;
